41ab433a9688f52a27fc0b4e13a11b7ec21f20a2
krzysztofhejna <khejna15@wp.pl>
2019-01-13 14:37:56 +0100

    Update program.js

--- app/program.js
+++ app/program.js
@@ -3,25 +3,26 @@ var OSinfo = require('../modules/OSinfo');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
   var input = process.stdin.read();
-  if (input !== null) {
-    var instruction = input.toString().trim();
-    switch (instruction) {
-      case '/exit':
-        process.stdout.write('Quitting app!\n');
-        break;
-      case '/version':
-        process.stdout.write('NodeJS version: ' + process.version + '\n');
-        break;
-      case '/language':
-        process.stdout.write('Language: ' + process.env.LANG + '\n');
-        break;
-      case '/getOSinfo':
-        OSinfo.print();
-        break;
-      default:
-        process.stderr.write('Wrong instruction\n');
-        break;
-    }
-  }
+  if (input == null) {
+    return;
+  };
+  var instruction = input.toString().trim();
+  switch (instruction) {
+    case '/exit':
+      process.stdout.write('Quitting app!\n');
+      break;
+    case '/version':
+      process.stdout.write('NodeJS version: ' + process.version + '\n');
+      break;
+    case '/language':
+      process.stdout.write('Language: ' + process.env.LANG + '\n');
+      break;
+    case '/getOSinfo':
+      OSinfo.print();
+      break;
+    default:
+      process.stderr.write('Wrong instruction\n');
+      break;
+  };
 });
 
